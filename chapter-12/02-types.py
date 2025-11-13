57d00bd86a60b80b5bb3e6c32311c56165ed0426
DEMIGOD1510 <ankitkrsingh151004@gmail.com>
2025-11-13 18:45:52 +0530

    Chapter-12 python

---
+++ chapter-12/02-types.py
@@ -0,0 +1,12 @@
+from typing import List, Union, _TypedDict
+
+
+
+n : int = 5
+
+name: str = "Ankit"
+
+
+def sum(a: int, b: int) -> int:
+  return a+b
+
---
+++ chapter-12/module.py
@@ -0,0 +1,9 @@
+def myFunc():
+  print("Hello World!")
+
+
+if __name__ == "__main__":
+  # If this code is directly executed by running the files its present in
+  print("We are directly running thid code")
+  myFunc()
+  print(__name__)
